6eedc0264d4f7aecfe3b0bc1f3ba0ce06ef6a9f8
Abdellatif Aghla <aaghla@student.1337.ma>
2023-12-20 21:45:18 +0100

    uloaded ;

---
+++ get_next_line_bonus.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaghla <aaghla@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 17:59:22 by aaghla            #+#    #+#             */
+/*   Updated: 2023/12/19 18:13:39 by aaghla           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*free_it(char *s)
+{
+	free(s);
+	return (NULL);
+}
+
+char	*read_line(int fd, char *reached)
+{
+	char	*buff;
+	int		rd;
+
+	rd = 2;
+	if (BUFFER_SIZE == 2147483647)
+		rd = 1;
+	buff = malloc((BUFFER_SIZE -1 + rd) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	while (!ft_strchr(reached, '\n') && rd > 0)
+	{
+		rd = read(fd, buff, BUFFER_SIZE);
+		if (rd == -1)
+		{
+			if (reached)
+				free(reached);
+			return (free_it(buff));
+		}
+		if (rd == 0)
+			break ;
+		buff[rd] = '\0';
+		reached = ft_strjoin(reached, buff, rd);
+	}
+	free(buff);
+	return (reached);
+}
+
+char	*one_line(char *reached)
+{
+	char	*line;
+	int		i;
+	int		j;
+	int		n;
+
+	i = 0;
+	j = -1;
+	if (!reached)
+		return (NULL);
+	while (reached[i] && reached[i] != '\n')
+		i++;
+	if (reached[i] == '\n')
+		n = 2;
+	else
+		n = 1;
+	line = malloc((i + n) * sizeof(char));
+	if (!line)
+		return (NULL);
+	while (++j < i)
+		line[j] = reached[j];
+	if (n == 2)
+		line[j++] = '\n';
+	line[j] = '\0';
+	return (line);
+}
+
+char	*next_line(char *reached)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!reached)
+		return (NULL);
+	else if (!reached[0] || !ft_strchr(reached, '\n'))
+		return (free_it(reached));
+	i = ft_strlen(reached, '\n');
+	j = ft_strlen(reached, '\0');
+	new = malloc((j + 1) * sizeof(char));
+	if (!new)
+		return (free_it(reached));
+	j = 0;
+	i = ft_strlen(reached, '\n') + 1;
+	while (reached[i])
+		new[j++] = reached[i++];
+	new[j] = '\0';
+	free(reached);
+	if (!new[0])
+		return (free_it(new));
+	return (new);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*reached[10240];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	reached[fd] = read_line(fd, reached[fd]);
+	line = one_line(reached[fd]);
+	reached[fd] = next_line(reached[fd]);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaghla <aaghla@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 18:00:38 by aaghla            #+#    #+#             */
+/*   Updated: 2023/12/19 12:09:36 by aaghla           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != (char)c)
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2, int rd)
+{
+	char	*str;
+	int		i;
+
+	if (!s1)
+		s1 = "\0";
+	i = ft_strlen(s1, 0);
+	str = (char *)malloc((i + rd +1) * sizeof(char));
+	if (!str)
+	{
+		if (s1[0])
+			free(s1);
+		return (NULL);
+	}
+	i = 0;
+	rd = 0;
+	while (s1[i])
+		str[rd++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[rd++] = s2[i++];
+	str[rd] = '\0';
+	if (s1[0])
+		free(s1);
+	return (str);
+}
+
+// #include <libc.h>
+// int main()
+// {
+// // 	int fd = open("fd1.txt", O_RDONLY);
+// // 	char *s = NULL;
+// // 	char p[BUFFER_SIZE + 1];
+// // 	p[BUFFER_SIZE] = '\0';
+// // 	int rd = read(fd, p, BUFFER_SIZE);
+// // 	char *r = ft_strjoin(s, p);
+// 	// printf("%s", r);
+
+// 	// char *s1 = malloc(sizeof(char) * 1);
+// 	// s1[0] = '\0';
+// 	// char *s2="hello";
+// 	// printf("%d", ft_strlen(s2));
+// }
